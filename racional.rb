668dbff0313fc3996318c72e5abd06e79fd8bea2
Mohammed Mahrach <alu0100777475@ull.edu.es>
2013-10-18 22:42:47 +0100

    subiendo la clase racional.rb

--- racional.rb
+++ racional.rb
@@ -1,6 +1,50 @@
-# Implementar en este fichero la clase para crear objetos racionales
+# Implementar en este fichero la clase para crear objetos Fracciones
 
 require "./gcd.rb"
 
 class Fraccion
+	
+	include Comparable
+	include Enumerable
+	
+	
+	def initialize(num, denom)
+		raise ZeroDivisionError, "El denominador debe ser mayor que cero" unless (denom != 0)
+		div = gcd(num, denom)
+		@num = num/div
+		@denom = denom/div
+	end
+	
+	attr_accessor :num, :denom
+	
+	def to_s
+		"#{@num}/#{@denom}"
+	end
+	
+	def +(o)
+		Fraccion.new(@num*o.denom + o.num*@denom, @denom*o.denom)
+	end
+	
+	def -(o)
+		Fraccion.new(@num*o.denom - o.num*@denom, @denom*o.denom)
+	end
+	
+	def *(o)
+		Fraccion.new(@num*o.num,@denom*o.denom)
+	end
+	
+	def /(o)
+		Fraccion.new(@num*o.denom,@denom*o.num)
+	end
+	
+	def ==(o)
+		if o.instance_of? Fraccion
+			@num == o.num && @denom == o.denom
+		else
+			false
+		end
+	end
+	
 end
+
+
